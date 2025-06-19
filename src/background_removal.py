4e250d1a272f33d88b76a261650be9185f7bda78
luckstrike <alexisgarc711@gmail.com>
2025-06-18 19:35:26 -0500

    Added in a way to test remove_background_to_image()

--- src/background_removal.py
+++ src/background_removal.py
@@ -31,3 +31,10 @@ class BackgroundRemover:
         image_bgr = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)
 
         return image_bgr, mask_array
+    
+if __name__ == "__main__":
+    INPUT_PATH = "../examples/input/giraffe.jpg"
+    OUTPUT_PATH = "../examples/output/giraffe_no_background.png"
+
+    bg_removal_obj = BackgroundRemover(INPUT_PATH, OUTPUT_PATH)
+    bg_removal_obj.remove_background_to_image()
