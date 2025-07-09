c93c1b3817e098209277fa7f5572d34379de4c0e
luckstrike <alexisgarc711@gmail.com>
2025-07-08 21:58:01 -0500

    Cleaned up the navbar a bit

--- frontend/src/lib/components/panels/navbar.svelte
+++ frontend/src/lib/components/panels/navbar.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import ModeToggle from "$lib/components/ui/modeToggle/modeToggle.svelte";
+	import Button from "../ui/button/button.svelte";
 </script>
 
-<ModeToggle />
+<div class="flex justify-end p-2 space-x-2">
+    <Button variant="outline" href="/">Home</Button>
+    <ModeToggle />
+</div>
