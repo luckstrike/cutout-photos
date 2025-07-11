232b18463567737b4287ee7a9ad4e0a066014207
luckstrike <alexisgarc711@gmail.com>
2025-07-10 23:05:03 -0500

    Added in a label for the imasge preview

--- frontend/src/lib/components/panels/selectImage.svelte
+++ frontend/src/lib/components/panels/selectImage.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import AspectRatio from "$lib/components/ui/aspect-ratio/aspect-ratio.svelte";
 	import UploadImage from "$lib/components/ui/uploadImage/uploadImage.svelte";
+	import Label from "../ui/label/label.svelte";
 </script>
 
 <div class="flex flex-col w-sm gap-2">
-    <AspectRatio ratio={1} class="border-2 rounded-lg bg-muted">
-        <img src="/images/giraffe-compressed.jpg" alt="..." class="w-full h-full rounded-lg object-cover" />
-    </AspectRatio>
-    <UploadImage />
+    <div class="flex flex-col gap-1">
+        <Label for="image-preview">Image Preview</Label>
+        <AspectRatio ratio={1} class="border-2 rounded-lg bg-muted" id="image-preview">
+            <img src="/images/giraffe-compressed.jpg" alt="..." class="w-full h-full rounded-lg object-cover" />
+        </AspectRatio>
+    </div>
+    <UploadImage/>
 </div>
