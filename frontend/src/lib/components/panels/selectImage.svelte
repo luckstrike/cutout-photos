909e73398ecf0dcdddbb72cd7fb2d97cb7ba4b46
luckstrike <alexisgarc711@gmail.com>
2025-07-11 22:55:49 -0500

    Image preview now reflects the selected image

--- frontend/src/lib/components/panels/selectImage.svelte
+++ frontend/src/lib/components/panels/selectImage.svelte
@@ -1,15 +1,42 @@
 <script lang="ts">
-	import AspectRatio from "$lib/components/ui/aspect-ratio/aspect-ratio.svelte";
-	import UploadImage from "$lib/components/ui/uploadImage/uploadImage.svelte";
-	import Label from "../ui/label/label.svelte";
+	import AspectRatio from '$lib/components/ui/aspect-ratio/aspect-ratio.svelte';
+	import UploadImage from '$lib/components/ui/uploadImage/uploadImage.svelte';
+	import Label from '../ui/label/label.svelte';
+
+    let maxFileSize : number = 5000000; // 5MB
+
+    let selectedFile : File | null = $state(null);
+    let imageURL : string | null = $derived(selectedFile ? URL.createObjectURL(selectedFile) : null);
+
+	function handleFileSelect(file: File) {
+        if (file && file.type.startsWith('image/') && file.size < maxFileSize) {
+            selectedFile = file;
+        } else {
+            // TODO: Change this to a Toast
+            alert('Please select a valid image under 5MB')
+        }
+    }
+
+    // Cleanup Function
+    $effect(() => {
+        return () => {
+            if (imageURL) {
+                URL.revokeObjectURL(imageURL);
+            }
+        };
+    });
 </script>
 
-<div class="flex flex-col w-sm gap-2">
-    <div class="flex flex-col gap-1">
-        <Label for="image-preview">Image Preview</Label>
-        <AspectRatio ratio={1} class="border-2 rounded-lg bg-muted" id="image-preview">
-            <img src="/images/giraffe-compressed.jpg" alt="..." class="w-full h-full rounded-lg object-cover" />
-        </AspectRatio>
-    </div>
-    <UploadImage/>
+<div class="w-sm flex flex-col gap-2">
+	<div class="flex flex-col gap-1">
+		<Label for="image-preview">Image Preview</Label>
+		<AspectRatio ratio={1} class="bg-muted rounded-lg border-2" id="image-preview">
+			<img
+				src={imageURL}
+				alt="..."
+				class="h-full w-full rounded-lg object-cover"
+			/>
+		</AspectRatio>
+	</div>
+	<UploadImage onFileSelect={handleFileSelect} />
 </div>
--- frontend/src/lib/components/ui/uploadImage/uploadImage.svelte
+++ frontend/src/lib/components/ui/uploadImage/uploadImage.svelte
@@ -2,10 +2,17 @@
 	import Button from "$lib/components/ui/button/button.svelte";
     import Input from "$lib/components/ui/input/input.svelte";
     import Upload from "@lucide/svelte/icons/upload";
-    import FileCheck from "@lucide/svelte/icons/file-check";
 	import Label from "../label/label.svelte";
 
+    let { onFileSelect } = $props();
     let files : FileList | undefined = $state(undefined);
+
+    $effect(() => {
+        if (files && files.length > 0) {
+            onFileSelect(files[0])       
+        }
+    });
+
 </script>
 
 <div class="flex w-full">
