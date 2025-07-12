09a707edabc902bc6497794585b79781aa83aaf3
luckstrike <alexisgarc711@gmail.com>
2025-07-11 23:09:42 -0500

    Removed upload image button

--- frontend/src/lib/components/ui/uploadImage/uploadImage.svelte
+++ frontend/src/lib/components/ui/uploadImage/uploadImage.svelte
@@ -16,37 +16,16 @@
 </script>
 
 <div class="flex w-full">
-    {#if files}
-        <div class="flex flex-col gap-1">
-            <Label for="picture">Select an Image to Upload</Label>
-            <div class="flex gap-1">
-                <Input 
-                    accept="image/*"
-                    bind:files
-                    class="flex"
-                    id="picture"
-                    type="file" 
-                />
-                <Button variant="outline" class="flex">
-                    <Upload />
-                </Button>
-            </div>
+    <div class="flex flex-col w-full gap-1">
+        <Label for="picture">Select an Image to Upload</Label>
+        <div class="flex flex-1 gap-1">
+            <Input 
+                accept="image/*"
+                bind:files
+                class="flex"
+                id="picture"
+                type="file" 
+            />
         </div>
-    {:else}
-        <div class="flex flex-col gap-1">
-            <Label for="picture">Select an Image to Upload</Label>
-            <div class="flex gap-1">
-                <Input 
-                    accept="image/*"
-                    bind:files
-                    class="flex text-gray-500"
-                    id="picture"
-                    type="file" 
-                />
-                <Button variant="outline" class="flex" disabled>
-                    <Upload />
-                </Button>
-            </div>
-        </div>
-    {/if}
+    </div>
 </div>
