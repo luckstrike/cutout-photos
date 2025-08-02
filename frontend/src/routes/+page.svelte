8ac384ff3060da9341712627aeadf2ff936d5037
luckstrike <alexisgarc711@gmail.com>
2025-08-02 00:04:05 -0500

    Started stubbing of a debounced cutout creation function

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,6 +3,10 @@
     import Navbar from "$lib/components/panels/navbar.svelte";
 	import SelectImage from "$lib/components/panels/selectImage.svelte";
 
+    import debounce from 'lodash.debounce';
+
+    let debounceTiming: number = 300; // in ms
+
     let fileObj: File | null = $state(null);
 
     let outlineThickness : number = $state(0);
@@ -31,6 +35,27 @@
         outlineColor = hexColor;
     }
 
+
+    $effect(() => {
+        // TODO: When a file and options are selected, we can now process something!
+        const createCutout = () => {
+            /* 
+             * 1. Reach out to REST API
+             * 2. Get cutout image result back 
+             * 3. Update the image shown by SelectImage
+             */
+        }
+
+        const debouncedCutout = debounce(createCutout, debounceTiming);
+
+        if (fileObj) {
+            debouncedCutout();
+        }
+
+        return () => {
+            debouncedCutout.cancel();
+        };
+    });
 </script>
 
 <div class="flex flex-col h-screen p-4 gap-4">
